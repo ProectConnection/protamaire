3d8494bfc8441c177cfbac0398f4360b80f49746
nwuser <nwuser@da.iwasaki.ac.jp>
2017-05-15 15:04:31 +0900

    ・椅子取りゲーム 椅子モドキのオブジェクト（赤・青・黄・緑）の周りをプレイヤー（白）が自動的に周回する。 コメントで隠してありますが、矢印キーで簡単な速度制限が出来ます。

---
+++ Assets/Isutori/Loop.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+
+
+    private float speed = 1;
+    private int speedCount = 0;
+    float movePower = 0.2f;
+
+    Rigidbody rd;
+
+    Rect _rect = new Rect(100, 100, 300, 300);
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rd = GetComponent<Rigidbody>();
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //A Dで移動
+        Input.GetAxis("Horizontal");
+        //this.transform.Rotate(0, Input.GetAxis("Horizontal") * 1 ) 0);
+
+        //Playerの自動移動
+        this.transform.Rotate(Time.deltaTime, speed, 0, 0);
+
+
+
+        //矢印キーでスピード制御
+        /*if (Input.GetKey("right"))
+          {
+              speed = 0.5f;
+          }
+          if (Input.GetKey("left"))
+          {
+              speed = 1f;
+          }*/
+
+
+    }
+
+        }
+
+
+
+
+
+
---
+++ Assets/Isutori/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    private int speed = 0;
+    float movePower = 0.2f;
+
+    Rigidbody rd;
+
+	// Use this for initialization
+	void Start () {
+
+        rd = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdata()
+    {
+
+        if (Input.GetKey("right"))
+        {
+
+            
+
+
+        }
+
+
+
+
+    }
+}
